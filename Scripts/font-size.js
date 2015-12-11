ed0287cb9f155d2f8ef687437569544250834c74
Oka Li <okaoka0709@gmail.com>
2015-12-12 00:45:11 +0800

    完成字級大小 js

--- Scripts/font-size.js
+++ Scripts/font-size.js
@@ -1,60 +1,36 @@
 define(['cookie', 'jquery', 'group'], function(cookie, $, group){
 
 	function main(env, opt, file){
-		var $li = group.getContentItem(env),
-			$li_length = $li.length;
+		var $li = $(group.getContentItem(env)), //取 li
+			$li_length = $li.length,
+			$a = $li.find('a'), //取按鈕
+			$root = $('.sys-root'); // root，class name 要放它身上
 
-		var _prefix = 'font-size-'; //前輟
-			
-		var $sizes = []; //存所有的 size class
+		var _eventNmae = 'reFontSize', //事件名稱
+			_prefix = 'font-size-', //前輟
+			_active = 'is-active'; //被選擇的 class name
 
-		for( var i = 0; i < $li_length; i++  ) {
-			$sizes.push( _prefix + $li[i].className);
-		}
+		var _result = cookie.get(_prefix) || $li.eq(Math.floor($li_length / 2)).attr('class'); //結果值，cookie 記錄 || 中間值
 
-		var $sizes_length = $sizes.length,
-			_defalt_value = Math.floor($sizes_length / 2);
-		
-		var _result = $sizes[_defalt_value]; //結果
+		$a.on(_eventNmae, function(){ //換字型大小事件
 
-		if (cookie.get(_prefix)){ _result = cookie.get(_prefix) } //找cookie，沒有就給預設值
+			var $this = $(this).closest('li');
 
-		var $root = document.querySelector('.sys-root');
+			_result = $this.removeClass(_active).attr('class'); //改變結果值
 
-		var $$li = $($li),
-			$a = $$li.find('a'),
-			_active = 'is-active';
-
-		$a.on('reFontSize', function(){
-
-			var $this = $(this).closest('li'),
-				_font_size = $this.attr('class');
-
-			$$li.removeClass(_active);
-			$this.addClass(_active);
-
-			_result = _prefix + _font_size;
-
-			for( var i = 0; i < $sizes_length; i++  ) {
-				var _regex = new RegExp($sizes[i]);
-
-				console.log($sizes[i])
-
-				$root.className = $root.className.replace(_regex,''); //比對刪除所有 size class
-			}
-
-			$root.className = $root.className + ' ' + _result;
+			$this.addClass(_active).siblings().removeClass(_active); //啟動項目添加 class
+			$root.removeClass(_prefix + cookie.get(_prefix)).addClass(_prefix + _result); //刪除 root 目前的字型大小，添加新的
 
 			cookie.set(_prefix, _result); //存 cookie
 		});
 
-		$a.on('click', function(evt){
+		$a.on('click', function(evt){ //click 觸發事件
 			evt.preventDefault();
 
-			$(this).trigger('reFontSize');
+			$(this).trigger(_eventNmae);
 		});
 
-		$a.eq(_defalt_value).trigger('reFontSize');
+		$li.filter('.'+ _result).find('a').trigger(_eventNmae); //執行 cookie 記錄或中間值
 	}
 	
 	return main;
