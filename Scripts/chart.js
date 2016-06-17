5fe826447bdd3fa071843aa7e433b328b54bcf44
Oka Li <okaoka0709@mail.hamastar.com.tw>
2016-06-17 17:57:36 +0800

    建構 high chart 模組方法
    1. 取消 script lib
    2. 初步建構 high chart 模組方法

---
+++ Scripts/chart.js
@@ -0,0 +1,20 @@
+define(['jquery'], function($){
+	
+	function main(env, opt, file){
+
+		var $set = {
+				debug: false
+			}
+
+		$.extend($set, opt);
+
+		console.log($set);
+
+		if($set.debug) {
+			console.log('預設值:', $set);
+			console.log('檔案 '+ file +'.js 已順利執行。');
+		}
+	}
+	
+	return main;
+});
