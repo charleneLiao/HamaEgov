e7bd542470246e9cf5b349e15f887244d8d7e6a3
Oka Li <okaoka0709@mail.hamastar.com.tw>
2016-09-26 18:47:11 +0800

    若干修改
    1. 修正模組框架
    2. 擴張 toogleBtn 功能
    3. 隱藏/顯示 page-search 及 site-map

--- Js/toogleBtn.js
+++ Js/toogleBtn.js
@@ -1,31 +1,49 @@
-define(function(){
+define(['getNode'], function(getNode){
 	
 	function main(env, opt, file){
 
 		var $set = {
 				targetNode: null,
-				targetClass: 'is-avtive',
-				thisClass: 'is-avtive',
+				targetClass: null,
+				thisClass: 'is-active',
+				btnText: null,
 				debug: false
 			}
 
 		$.extend($set, opt);
 
-		var $target = $(opt.targetNode),
-			$env = $(env),
-			$a = $env.find('a');
-
-		if( !$target.length ) { $href = $($a.attr('href')) }
-
 		var _eventNmae = file; //事件名稱
 
-		$a.on(_eventNmae, function(){
+		var $env = $(env),
+			$target = $($set.targetNode),
+			_class = $env.attr('class'),
+			$btn = null;
+
+		console.log()
+
+		if( _class.match('simple-text') || _class.match('simple-pic') ) { //如果是簡單文字或簡單圖片系列
+			$btn = $env.find('a');
+		}else {
+			$btn = getNode.getFtItemBtn(env, 'more').find('a');
+		}
+
+		if( $set.btnText !== null ) {
+			$btn.text($set.btnText);
+		}
+
+		var $env = $(env);
+
+		if( !$target.length ) { //沒有設目標的話，就是自己的 ct
+			$target = getNode.getCt(env);
+		}
+
+		$btn.on(_eventNmae, function(){
 
 			$target.toggleClass($set.targetClass);
 			$env.toggleClass($set.thisClass);
 		});
 
-		$a.on('click', function(evt){ //觸發事件
+		$btn.on('click', function(evt){ //觸發事件
 			evt.preventDefault();
 
 			$(this).trigger(_eventNmae);
