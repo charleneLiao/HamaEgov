9b3982d78307c86d171556bc1f105f3ff6c9107e
Oka Li <okaoka0709@mail.hamastar.com.tw>
2016-10-12 15:41:10 +0800

    新增電子書

--- Js/tab.js
+++ Js/tab.js
@@ -3,12 +3,17 @@ define(['getNode'], function(getNode){
 	function main(env, opt, file){
 
 		var $set = {
+				auto: false, //是否自動撥放
+				delay: 5000, //停留時間
+				event: 'click focusin', //jQuery 事件名稱
+				activeClass: 'is-active', //啟動點點的 class
 				debug: false
 			}
 
 		$.extend($set, opt);
 
-		var $all_item = getNode.getCtItem(env),
+		var $env = $(env),
+			$all_item = getNode.getCtItem(env),
 			$items = $all_item.filter(function(i) { //過濾第一個 jquery 物件，也就是 tab
 				return !!i;
 			}),
@@ -18,7 +23,8 @@ define(['getNode'], function(getNode){
 			$tab_a = $tabs_li.find('a'),
 			$tab_a_length = $tab_a.length - 1;
 
-		var _tab_key = 9;
+		var _tab_key = 9,
+			_index = 0;
 
 		$items.each(function(i, n){
 			var $this = $(this),
@@ -68,25 +74,52 @@ define(['getNode'], function(getNode){
 		}
 
 		var _eventNmae = file, //事件名稱
-			_active = 'is-active'; //被選擇的 class name
+			_active = $set.activeClass; //被選擇的 class name
 
 		$tab_a.on(_eventNmae, function(){
-			var $this = $(this),
-				_index = $this.closest('li').index();
+			var $this = $(this);
 
-			$tabs_li.removeClass(_active);
-			$tabs_li.eq(_index).addClass(_active);
-			$items.hide();
-			$items.eq(_index).show();
+			_index = $this.closest('li').index();
+
+			slider(_index);
 		});
 
-		$tab_a.on('click focusin', function(evt){ //觸發事件
+		$tab_a.on( $set.event, function(evt){ //觸發事件
 			evt.preventDefault();
 
 			$(this).trigger(_eventNmae);
 		});
 
-		$tab_a.eq(0).trigger(_eventNmae); //先點擊，同時用 css 先做掉
+		slider(0);
+
+		function slider(_index) {
+			$tabs_li.removeClass(_active);
+			$tabs_li.eq(_index).addClass(_active);
+			$items.hide();
+			$items.eq(_index).show();
+		}
+
+		if ($set.auto) { //如果要輪播
+			var timer; //設定計時器
+
+			function auto() { //設定自動撥放涵式
+
+				_index = (_index + 1 + $items_l) % $items_l; //算出第幾個要被撥放
+
+				slider(_index); //預設向右撥放
+				timer = setTimeout(auto, $set.delay);
+			}
+			
+			$env.on('mouseenter', function(){ //設定滑進滑出項目
+				clearTimeout(timer);
+			});
+
+			$env.on('mouseleave', function(){
+				timer = setTimeout(auto, $set.delay);
+			});
+
+			timer = setTimeout(auto, $set.delay); //輪播開始
+		}
 
 		if($set.debug) {
 			console.log('預設值:', $set);
