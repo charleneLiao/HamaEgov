73d5ed9a76401c95392a3d3323390a3a7139c30a
Oka Li <okaoka0709@mail.hamastar.com.tw>
2016-10-20 17:19:05 +0800

    1. 增加輪播 04 2. 修正 bug

--- Js/flexslider.js
+++ Js/flexslider.js
@@ -1,14 +1,7 @@
 define(['flexslider/jquery.flexslider'], function(getNode){
 
 	function main(env, opt, file){
-
-		/*
-		 * jQuery FlexSlider v2.6.3
-		 * Copyright 2012 WooThemes
-		 * Contributing Author: Tyler Smith
-		 */
-
-		$(env).find('.'+ file)[file](opt);
+		$(env).find('.flexslider').flexslider(opt);
 	}
 	
 	return main;
