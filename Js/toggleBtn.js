719e53282a793a03c1e66dcbd6b3ca7ec9a322e4
Oka Li <okaoka0709@mail.hamastar.com.tw>
2016-11-14 14:19:51 +0800

    新增功能
    1. 整併 toggleBtn 與 targetBtn
    2. 完成 cookie 功能與替換字功能設定
    3. 修改 footmenu 樣式
    4. 修改版型配置檔

--- Js/toggleBtn.js
+++ Js/toggleBtn.js
@@ -3,11 +3,14 @@ define(['getNode'], function(getNode){
 	function main(env, opt, file){
 
 		var $set = {
-				bindNode: 'hd',
+				bindNode: '.hd',
+				targetNode: null,
+				targetClass: 'is-active',
 				toggleClass: 'is-active',
 				btnOrangeText: null,
 				btnActiveText: null,
 				event: 'click', //jQuery 事件名稱
+				cookie: false,
 				debug: false
 			}
 
@@ -16,26 +19,34 @@ define(['getNode'], function(getNode){
 		var _eventNmae = file; //事件名稱
 
 		var $env = $(env),
-			$target = $($set.targetNode),
-			$btn = null; //按鈕物件
+			$btn = null, //按鈕物件
+			$target = $($set.targetNode);
 
-		var _uuid = $env.attr('class').replace($set.toggleClass, ''),
-			_flag = $.cookie(_uuid) || 0; //0 未執行 / 1 執行中
+		var _uuid = $env.attr('class').replace($set.toggleClass, ''), //!!!!----要想一個 UUID 方法
+			_flag = null; //0 未執行 / 1  執行中 / null 沒有
 
-		console.log(_uuid, $.cookie(_uuid), _flag);
-
-		if( _flag ) {
-			$env.addClass($set.toggleClass);
+		if( !$set.cookie ) {
+			$.cookie(_uuid, null);
 		}else {
-			$env.removeClass($set.toggleClass);
-
-			console.log($env.removeClass($set.toggleClass))
+			_flag = $.cookie(_uuid);
 		}
 
-		if( $set.bindNode === 'hd' ) { //版頭抑或綁尾
+		if( _flag === '1' ) {
+			$target.addClass($set.targetClass);
+			$env.addClass($set.toggleClass);
+		}else if( _flag === '0' ) {
+			$target.removeClass($set.targetClass);
+			$env.removeClass($set.toggleClass);
+		}
+
+		if( $set.bindNode === '.hd' ) { //綁頭、身體或尾巴
 			$btn = getNode.getHdLink(env).find('a');
-		}else if ( $set.bindNode === 'ft' ){
+		}else if ( $set.bindNode === '.ct' ){
+			$btn = getNode.getCtIn(env).find('a');
+		}else if ( $set.bindNode === '.ft' ){
 			$btn = getNode.getFtItemBtn(env).find('a');
+		}else {
+			$btn = $env.find($set.bindNode);
 		}
 
 		if( !!$set.btnOrangeText && _flag && !!$set.btnActiveText ) { //如果有設定文字
@@ -44,16 +55,19 @@ define(['getNode'], function(getNode){
 			$btn.text($set.btnOrangeText);
 		}
 
+		console.log($btn.text());
+
 		$btn.on(_eventNmae, function(){
+			$target.toggleClass($set.targetClass);
 			$env.toggleClass($set.toggleClass);
 
-			if( $env.attr('class').search($set.toggleClass) > -1 ) {
-				$.cookie(_uuid, 1);
-			}else {
-				$.cookie(_uuid, 0);
+			if( $env.attr('class').search($set.toggleClass) > -1 && $set.cookie ) { //如果有開啟 cookie 功能就紀錄
+				$.cookie(_uuid, '1');
+			}else if ( !!$set.cookie ){
+				$.cookie(_uuid, '0');
 			}
 
-			console.info($.cookie(_uuid));
+			console.log($btn.text(), $set.btnOrangeText );
 
 			if( $btn.text() === $set.btnOrangeText && !!$set.btnActiveText ) { //更改文字
 				$btn.text($set.btnActiveText);
